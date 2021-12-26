06bd5709788e265270e432e289f70ea8804a837d
Alireza_Metanat <alirezafriends5@gmail.com>
2021-12-26 16:48:37 +0330

    get note

---
+++ NoteProject/NoteProject/Dto/Note/GetNoteDto.cs
@@ -0,0 +1,15 @@
+﻿using NoteProject.Dto.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteProject.Dto.Note
+{
+    public class GetNoteDto: TokenDto
+    {
+        public int Id { get; set; }
+        public bool IsClientSide { get; set; }
+
+    }
+}
